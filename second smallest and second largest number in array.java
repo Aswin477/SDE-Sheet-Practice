dc180865a2aed349d6e39667b35099243dc55ed4
ArunPrasanth-V <aruncuck111@gmail.com>
2022-04-09 06:46:25 +0530

    program

---
+++ second smallest and second largest number in array.java
@@ -0,0 +1,19 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int arr1[]={1,5,6,9,12,16};
+	    for(int i=0;i<arr1.length;i++)
+	    {
+	        System.out.println();
+	    }
+	    if(arr1[0]==arr1[1])
+	    {
+	        System.out.println("-1");
+	    }
+	    else{
+	        System.out.println(arr1[0]);
+	        System.out.println(arr1[1]);
+	    }
+	    
+	}
+}
