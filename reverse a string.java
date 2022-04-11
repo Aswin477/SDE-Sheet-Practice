448e59dbb1939639b8f11ce01aedd6c9d4498cea
ArunPrasanth-V <aruncuck111@gmail.com>
2022-04-11 07:02:06 +0530

    program

---
+++ reverse a string.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String word="Benjamin";
+	    String n1[]=word.split("");
+	    for(int i=n1.length-1;i>=0;i--){
+	     System.out.print(n1[i]+"");
+	   
+	    }
+	    
+	}	
+}
