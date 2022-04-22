506f24bf328716384873c0f752ef01e4c03d5cdc
ArunPrasanth-V <aruncuck111@gmail.com>
2022-04-22 22:28:01 +0530

    Arraylist

---
+++ adding elements using arraylist.java
@@ -0,0 +1,14 @@
+package demo1;
+
+import java.util.ArrayList;
+
+public class Hell {
+
+	public static void main(String[] args) {
+		ArrayList <String> st=new ArrayList <String>();
+		st.add("PHP");
+		st.add("HTML");
+		System.out.println(st);
+	}
+
+}
