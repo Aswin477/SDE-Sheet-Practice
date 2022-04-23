01edca2ac0c0c8648ea5437c562c1569c09cc234
ArunPrasanth-V <aruncuck111@gmail.com>
2022-04-23 07:33:04 +0530

    ArrayList

---
+++ deleting all strings.java
@@ -0,0 +1,16 @@
+package demo1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Hell {
+
+	public static void main(String[] args) {
+		ArrayList <String> names=new ArrayList <String>(Arrays.asList("Clint","Ben","Lois","Chris"));
+		names.clear();
+		System.out.println(names);
+		
+	}
+
+}
+
