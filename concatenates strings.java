5401faa7b8d13cf027f45e8efd382a698e6b1135
ArunPrasanth-V <aruncuck111@gmail.com>
2022-04-30 18:18:16 +0530

    Strings

---
+++ concatenates strings.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+	String str="louis";
+		 String str2="griffin ";
+		 String s3=str2.concat(str);
+		 
+			System.out.println(s3);
+		
+	}
+}
+
