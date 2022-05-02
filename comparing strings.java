17774e31cc82a2c60a6d853a922aa098b4de2c37
ArunPrasanth-V <aruncuck111@gmail.com>
2022-05-02 19:52:39 +0530

    Strings

---
+++ comparing strings.java
@@ -0,0 +1,13 @@
+package demo1;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       String s="lekumia";
+       String s1="lekophobia";
+       System.out.println(s.compareTo(s1));
+	}
+	
+}
+
