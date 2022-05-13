54fd6b57df72157fc073dea418251a0f66cddb30
ArunPrasanth-V <aruncuck111@gmail.com>
2022-05-13 22:06:16 +0530

    Pattern

---
+++ Right angle pattern.java
@@ -0,0 +1,20 @@
+package demo1;
+
+public class Practice2 {
+	 public static void main(String[] args) {
+	int n=6;
+    for(int i=0;i<n;i++)
+    {
+    	
+    	for(int j=i;j>0;j--) {
+    		System.out.print("$");
+        }
+    	 System.out.println();
+    	}
+   
+    	 
+   
+	}
+}
+
+
