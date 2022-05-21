4f52a9ca013f13d935285ff8620cbf923537b17e
ArunPrasanth-V <aruncuck111@gmail.com>
2022-05-21 21:15:16 +0530

    ASCII

---
+++ ASCII value.java
@@ -0,0 +1,17 @@
+package demo1;
+
+import java.util.Scanner;
+
+public class Practice2 {
+	 public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the character");
+	char c=sc.next().charAt(0);
+	System.out.println((int)c);
+	
+		
+	}
+	
+ }
+
+
