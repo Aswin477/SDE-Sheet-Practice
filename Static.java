f31485b4e0dda1072f1fdc80a89f9ca8bee8ab1d
ArunPrasanth-V <aruncuck111@gmail.com>
2022-05-24 18:30:15 +0530

    static

---
+++ Static.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Static {
+static
+{
+	System.out.println("static block");
+}
+	public static void main(String[] args) {
+
+		System.out.println("Main block");
+		
+
+	}
+
+}
+
