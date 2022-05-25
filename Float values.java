874c38e38066afa2a583cfa054650232d286599a
ArunPrasanth-V <aruncuck111@gmail.com>
2022-05-25 18:03:31 +0530

    Specifiers

---
+++ Float values.java
@@ -0,0 +1,16 @@
+
+package demo1;
+
+import java.util.Scanner;
+
+
+public class practice {
+
+	public static void main(String[] args) {
+
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter a float value");
+		float num=s.nextFloat();
+		System.out.format("%.3f",num);
+		}
+		}
