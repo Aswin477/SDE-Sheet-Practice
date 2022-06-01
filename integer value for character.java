ec2c3f593186d922154d3925943efd728b7ccf68
ArunPrasanth-V <aruncuck111@gmail.com>
2022-06-01 19:59:36 +0530

    Character

---
+++ integer value for character.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+    Scanner s=new Scanner(System.in);
+    System.out.println("Enter the character");
+    char ch=s.next().charAt(0);
+    System.out.println(ch-96);
+	}
+}
+
