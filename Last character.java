725d038c4282c2fe5d0869c29ed872fe7e6a2107
ArunPrasanth-V <aruncuck111@gmail.com>
2022-06-03 18:56:42 +0530

    String

---
+++ Last character.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter text");
+		String str=s.next();
+		System.out.println(str.substring(str.length()-1));
+		
+	}
+}
+
